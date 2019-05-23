d2a90f9343d8552f4638197dcae6d99ffb9c9031
Carlos Meza <38411439+cmeza432@users.noreply.github.com>
2019-05-23 11:53:56 -0500

    Update shelvingFilter.py

--- shelvingFilter.py
+++ shelvingFilter.py
@@ -1,7 +1,8 @@
 """
 Name:           Carlos Meza
 Description:
- 
+ Use the shelving filter on the wav file, then use fft on the original wav and then after the shelving filter. Then plot
+ both signals and output the clean file using only real values.
 """
 
 
